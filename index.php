c7e7d43d2b7a02a36d9b93761a144e212076d5af
stagiaire <stagiaire@iMac6.local>
2017-01-16 16:04:28 +0100

    Start project with form

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php?>
+<form method="post" action="action.php">
+  <p>Name of your database :</p>
+  <input type="text" name="dbname"/>
+  <p>User :</p>
+  <input type="text" name="dbuser" value="">
+  <p>Password :</p>
+  <input type="text" name="dbpwd" value="">
+  <p>Your host data base :</p>
+  <input type="text" name="dbhost" value="">
+  <p>Do you want a prefix for you wp tables ?</p>
+  <input type="checkbox" name="" value="">
+  <p>Your website name ?</p>
+  <input type="text" name="sitename" value="">
+  <p>Site URL ?</p>
+  <input type="text" name="wpurl" value="">
+  <p>WP admin user :</p>
+  <input type="text" name="wpuser" value="">
+  <p>WP password :</p>
+  <input type="text" name="wpwd" value="">
+  <p>Email ?</p>
+  <input type="mail" name="email" value="">
+  <p>SE visibility ?</p>
+  <input type="checkbox" name="visibility" value="">
+</form>
